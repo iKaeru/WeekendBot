58544805c94d8e00677b5fe99d7123ac1486dd72
iKaeru <antitery@mail.ru>
2022-03-08 02:30:08 +0300

    Sticker info class added

---
+++ Weekend/Models/StickerInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Weekend.Models
+{
+	public class StickerInfo
+	{
+		public int CurrentCounter;
+		public readonly List<string> TriggerPhrases;
+		public string FileId { get; }
+
+		public StickerInfo(string fileId, int currentCounter, List<string> triggerPhrases)
+		{
+			FileId = fileId;
+			CurrentCounter = currentCounter;
+			TriggerPhrases = triggerPhrases;
+		}
+	}
+}
